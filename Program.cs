ef3c85c1f59286b82e0fa39826806db93ecf305f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-25 19:47:04 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿String pudim;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Escreva uma frase");
+Console.ResetColor();
+pudim = Console.ReadLine()!.Replace("r", "l");
+Console.WriteLine($"{pudim}");
+
